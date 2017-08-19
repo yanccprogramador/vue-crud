26ad0e9501f011b40019a12b47c7e3700f87c584
yanccprogramador <yandepaula@gmail.com>
2017-08-19 16:07:28 -0300

    making crud

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,35 @@
 <template>
-  <div id="hello">
-    <img src="http://vuejs.org/images/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+  <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6" >
+    <form v-on:submit.prevent="create">
+    <div class="form-group">
+      <label for="nome">Nome</label>
+      <input type="text" id="nome" v-model="product.nome">
+    </div>
+    <div class="form-group">
+      <label for="quantidade">Quantidade</label>
+      <input type="text" id="quantidade" v-model="product.quantidade" >
+
+    </div>
+    <button class="btn btn-primary" >Salvar</button>
+    </form>
+    <table class="table">
+      <thead>
+          <td>Quantidade</td>
+          <td>Nome</td>
+      </thead>
+      <tr v-for="product in txt">
+                    <td>{{ product.quantidade }}</td>
+                    <td>{{ product.nome }}</td>
+                    <td><button class="btn btn-danger" v-on:click="del(product.id)">Excluir</button></td>
+
+      </tr>
+    </table>
   </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -24,37 +37,44 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      txt:[],
+      product:{}
     }
-  }
+  },
+  created:function(){
+      this.fetchProductData();
+  },
+  methods: {
+            fetchProductData: function()
+            {
+                this.$http.get('http://localhost:3000/api/products').then((response) => {
+                    this.txt = response.body;
+
+                }, (response) => {
+                });
+            },
+          del: function(id)
+          {
+              this.$http.delete('http://localhost:3000/api/products/'+id).then((response) => {
+                      this.fetchProductData();
+
+              }, (response) => {
+              });
+          },
+        create: function()
+        {
+            this.$http.post('http://localhost:3000/api/products/', this.product, {
+                    headers : {
+                        'Content-Type' : 'application/json'
+                    }
+                }).then((response) => {
+                    this.fetchProductData();
+
+            }, (response) => {
+            });
+        }
+      }
+
 }
 </script>
-
-<style scoped>
-#hello {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 // This is done with the browser options. For the config, see package.json
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
 new Vue({ // eslint-disable-line no-new
   el: '#app',
   render: (h) => h(App),
+  
 })
